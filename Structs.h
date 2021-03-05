dccaf9c1b4f768248931697171ced4813ea9607d
mr.Escow <escowxbox@gmail.com>
2021-03-05 19:45:34 +0300

    update 8

--- Structs.h
+++ Structs.h
@@ -7,25 +7,7 @@ struct Pair {
 
 
     Pair(int n, ...) {
-        //
-        //int* p = &n;
-        //int count = 0;
 
-        //while (*p != 2) {
-        //    count++;
-        //    p++;
-        //}
-
-        //p -= count;
-
-        //size = count;
-        //int* arr_10 = new int[size];
-        //arr = arr_10;
-
-        //for (int i = 0; i < count; ++i) {
-        //    arr[i] = !*p;
-        //    p++;
-        //}
     }
 
     void create_Not_Bool_Arrey(int n, ...) {
